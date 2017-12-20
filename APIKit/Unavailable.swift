a68445b4e77ab5f021abc873c1dea111e4401a82
yochidros <mm9.movement.trb@gmail.com>
2017-12-20 19:00:16 +0900

    edit  unavailable

--- APIKit/Unavailable.swift
+++ APIKit/Unavailable.swift
@@ -34,5 +34,17 @@ extension Session {
         fatalError("\(#function) is no longer available")
     }
     
+    @available(*, unavailable, renamed: "send(_:callbackQueue:handler:)")
+    public func sendRequest<Request: APIKit.Request>(_ request: Request, callbackQueue: CallbackQueue? = nil, handler: @escaping (Result<Request.Response, SessionTaskError>) -> Void = { _ in }) -> SessionTask? {
+        fatalError("\(#function) is no longer available")
+    }
     
-}
+    @available(*, unavailable, renamed: "cancelRequests(with:passingTest:)")
+    public class func cancelRequest<Request: APIKit.Request>(_ requestType: Request.Type, passingTest test: @escaping (Request) -> Bool = { _ in true }) {
+        fatalError("\(#function) is no longer available")
+    }
+    
+    @available(*, unavailable, renamed: "cancelRequests(with:passingTest:)")
+    public func cancelRequest<Request: APIKit.Request>(_ requestType: Request.Type, passingTest test: @escaping (Request) -> Bool = { _ in true }) {
+        fatalError("\(#function) is no longer available")
+    }}
