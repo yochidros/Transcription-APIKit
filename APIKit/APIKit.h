b307f4f23e073de0bb3d8a5d6f3639e5f6c9bf4c
yochidros <mm9.movement.trb@gmail.com>
2017-12-20 11:51:49 +0900

    Create APIKit framework 枠だけ

---
+++ APIKit/APIKit.h
@@ -0,0 +1,19 @@
+//
+//  APIKit.h
+//  APIKit
+//
+//  Created by Yoshiki Miyazawa on 2017/12/20.
+//  Copyright © 2017年 yochio. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for APIKit.
+FOUNDATION_EXPORT double APIKitVersionNumber;
+
+//! Project version string for APIKit.
+FOUNDATION_EXPORT const unsigned char APIKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <APIKit/PublicHeader.h>
+
+
