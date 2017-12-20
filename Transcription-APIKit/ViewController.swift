060886b157473f729b28d81b871dd5a48383db98
yochidros <mm9.movement.trb@gmail.com>
2017-12-20 10:53:03 +0900

    Initial Commit

---
+++ Transcription-APIKit/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Transcription-APIKit
+//
+//  Created by Yoshiki Miyazawa on 2017/12/20.
+//  Copyright © 2017年 yochio. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
